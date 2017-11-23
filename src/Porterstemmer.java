701cd64d8d70204d0ff34e32ffaf81908e2bbf3f
Explorer001 <lukas.gehreke@gmail.com>
2017-11-23 23:00:34 +0100

    Everthing should work except check vor CVC

--- src/Porterstemmer.java
+++ src/Porterstemmer.java
@@ -1,27 +1,26 @@
 package ir.stemmer;
 
 public class Porterstemmer {
+	
+	static char NOT_VOWEL = 'b';
 
 	public static void main(String[] args) {
 		Porterstemmer ps = new Porterstemmer();
-		System.out.println(ps.stemm("controlling"));
+		System.out.println(ps.stemm("filling"));
 	}
 	
 	public Porterstemmer() {}
 	
 	public String stemm(String term) {
-		int m;
 		String[][] replacements_s1a = {{"sses", "ss"},{"ies", "i"},{"ss", "ss"},{"s",""}};
 		String[][] replacements_s2 = {{"ational", "ate"}, {"tional", "tion"}, {"enci", "ence"}, {"anci", "ance"}, {"izer", "ize"}, {"abli", "able"}, {"alli", "al"}, {"entli", "ent"}, {"eli", "e"}, {"ousli", "ous"}, {"ization", "ize"}, {"ation", "ate"}, {"ator", "ate"}, {"alism", "al"}, {"iveness", "ive"}, {"fulness", "ful"}, {"ousness", "ous"}, {"aliti", "al"}, {"iviti", "ive"}, {"biliti", "ble"}};
 		String[][] replacements_s3 = {{"icate", "ic"}, {"ative", ""}, {"alize", "al"}, {"iciti", "ic"}, {"ical", "ic"}, {"ful", ""}, {"ness", ""}};
 		
-		term = replace(replacements_s1a, term);
+		term = replace(replacements_s1a, term, -1);
 		term = step1b(term);
 		term = step1c(term);
-		m = getVCCount(term);
-		if (m > 0) term = replace(replacements_s2, term);
-		m = getVCCount(term);
-		if (m > 0) term = replace(replacements_s3, term);
+		term = replace(replacements_s2, term, 0);
+		term = replace(replacements_s3, term, 0);
 		term = step4(term);
 		term = step5a(term);
 		term = step5b(term);
@@ -37,7 +36,7 @@ public class Porterstemmer {
 		boolean is_vowel;
 		for (int i = 0; i < term.length(); i++) {
 			c = term.charAt(i);
-			c1 = (i > 0)? term.charAt(i-1): 'b'; //b because it would not break y
+			c1 = (i > 0)? term.charAt(i-1): NOT_VOWEL; //b because it would not break y
 			is_vowel = isVowel(c, c1);
 			if (is_vowel && got_vowel == false) { //char is vowel
 				got_vowel = true;
@@ -74,7 +73,7 @@ public class Porterstemmer {
 		char c, c1;
 		for (int i = 0; i < term.length(); i++) {
 			c = term.charAt(i);
-			c1 = (i > 0)? term.charAt(i-1): 'b'; //b because it would not break y
+			c1 = (i > 0)? term.charAt(i-1): NOT_VOWEL; //b because it would not break y
 			if (isVowel(c,c1)) return true;
 		}
 		return false;
@@ -87,31 +86,38 @@ public class Porterstemmer {
 	
 	private static boolean endsOnCVC(String term) {
 		int m = getVCCount(term);
-		if ((m > 0) && !(isVowel(term.charAt(0), 'b')) && !(isVowel(term.charAt(term.length()-1), term.charAt(term.length()-2)))) {
-			char ch = term.charAt(term.length()-1);
-			if (ch == 'w' || (ch == 'x') || (ch == 'y')) return false;
+		char start = term.charAt(0);
+		char last = term.charAt(term.length()-1);
+		char lastm1 = term.charAt(term.length()-2);
+		if (((m > 0) && !isVowel(start, NOT_VOWEL) && !isVowel(last, lastm1)) || ((m > 1) && !isVowel(last, lastm1))) {
+			if (last == 'w' || (last == 'x') || (last == 'y')) return false;
 			return true;
 		}
 		return false;
 	}
 	
-	private String replace(String[][] repl, String term) {
+	private String replace(String[][] repl, String term, int condition) {
 		term = term.toLowerCase();
+		String stem;
 		for (int i = 0; i < repl.length; i++) {
 			if (term.endsWith(repl[i][0])) {
 				//regex match end of line
-				return term.replaceFirst(repl[i][0] + "$", repl[i][1]);
+				stem = term.replaceFirst(repl[i][0] + "$", repl[i][1]);
+				if(getVCCount(stem) > condition) return stem;
 			}
 		}
 		return term;
 	}
 	
 	private String step1b(String term) {
+		boolean twoOrThree = false;
 		term = term.toLowerCase();
 		int m = getVCCount(term);
-		if (m > 0) {
-			if (term.endsWith("eed")) {
-				term = term.replaceFirst("eed$", "ee");
+		String stem;
+		if (term.endsWith("eed")) {
+			stem = term.replaceFirst("eed$", "ee");
+			if (getVCCount(stem) > 0) {
+				term = stem;
 				m = getVCCount(term);
 			}
 		}
@@ -119,28 +125,32 @@ public class Porterstemmer {
 			if (term.endsWith("ed")) {
 				term = term.replaceFirst("ed$", "");
 				m = getVCCount(term);
+				twoOrThree = true;
 			} else if (term.endsWith("ing")) {
 				term = term.replaceFirst("ing$", "");
 				m = getVCCount(term);
+				twoOrThree = true;
 			}
 		}
-		if (term.endsWith("at")) {
-			term = term.replaceFirst("at$", "ate");
-			m = getVCCount(term);
-		} else if (term.endsWith("bl")) {
-			term = term.replaceFirst("bl$", "ble");
-			m = getVCCount(term);
-		} else if (term.endsWith("iz")) {
-			term = term.replaceFirst("iz$", "ize");
-			m = getVCCount(term);
-		}
-		if (endsOnDouble(term) && !(term.endsWith("l") || term.endsWith("s") || term.endsWith("z"))) {
-			term = term.substring(0, term.length() - 1); //cut last element
-			m = getVCCount(term);
-		}
-		if ((m == 1) && endsOnCVC(term)) {
-			term += "e";
-			m = getVCCount(term);
+		if (twoOrThree) {
+			if (term.endsWith("at")) {
+				term = term.replaceFirst("at$", "ate");
+				m = getVCCount(term);
+			} else if (term.endsWith("bl")) {
+				term = term.replaceFirst("bl$", "ble");
+				m = getVCCount(term);
+			} else if (term.endsWith("iz")) {
+				term = term.replaceFirst("iz$", "ize");
+				m = getVCCount(term);
+			}
+			if (endsOnDouble(term) && !(term.endsWith("l") || term.endsWith("s") || term.endsWith("z"))) {
+				term = term.substring(0, term.length() - 1); //cut last element
+				m = getVCCount(term);
+			}
+			if ((m == 1) && endsOnCVC(term)) {
+				term += "e";
+				m = getVCCount(term);
+			}
 		}
 		return term;
 	}
@@ -152,12 +162,15 @@ public class Porterstemmer {
 	
 	private String step4(String term) {
 		String[][] repl = {{"al", "0"}, {"ance", "0"}, {"ence", "0"}, {"er", "0"}, {"ic", "0"}, {"able", "0"}, {"ible", "0"}, {"ant", "0"}, {"ement", "0"}, {"ment", "0"}, {"ent", "0"}, {"ion", "1"}, {"ou", "0"}, {"ism", "0"}, {"ate", "0"}, {"iti", "0"}, {"ous", "0"}, {"ive", "0"}, {"ize", "0"}};
-		int m = getVCCount(term);
+		int m;
+		String stem;
 		for (int i = 0; i < repl.length; i++) {
 			if (term.endsWith(repl[i][0])) {
-				if (m > 1 && repl[i][1] == "0") return term.replaceFirst(repl[i][0] + "$", "");
+				stem = term.replaceFirst(repl[i][0] + "$", "");
+				m = getVCCount(stem);
+				if (m > 1 && repl[i][1] == "0") return stem;
 				else {
-					if (m > 1 && (term.endsWith("s") || term.endsWith("t"))) return term.replaceFirst(repl[i][0] + "$", "");
+					if (m > 1 && (stem.endsWith("s") || stem.endsWith("t"))) return stem;
 				}
 			}
 		}
@@ -165,15 +178,16 @@ public class Porterstemmer {
 	}
 	
 	private String step5a(String term) {
-		int m = getVCCount(term);
-		if (m > 1) return term.replaceFirst("e$", "");
-		else if (m == 1 && !term.endsWith("o")) return term.replaceFirst("e$", "");
+		String stem = term.replaceFirst("e$", "");
+		int m = getVCCount(stem);
+		if (m > 1) return stem;
+		else if (m == 1 && !endsOnCVC(stem)) return stem;
 		return term;
 	}
 	
 	private String step5b(String term) {
 		int m = getVCCount(term);
-		if (m > 1 && endsOnDouble(term) && term.endsWith("l")) term = term.substring(0, term.length()-1);
+		if (m > 1 && endsOnDouble(term) && term.endsWith("l")) return term.substring(0, term.length()-1);
 		return term;
 	}
 
